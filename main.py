622b1a228bfd5ea270129b0886f85be53d0d0f15
Hao-Min Liu (Paul) <liuhaumin@gmail.com>
2018-12-04 14:32:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,57 +20,37 @@ from config import *
 if __name__ == '__main__':
 
     """ Create TensorFlow Session """
-
-    t_config = TrainingConfig
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = t_config.gpu_num
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-
-    with tf.Session(config=config) as sess:
-
-        #path_x_train_phr =  'tra_X_phrase_all' # (50266, 384, 84, 5)
-        #path_x_train_phr =  'tra_X_phrs' # (5990, 384, 84, 2)
+    with tf.Session() as sess:
+        
+    # === Prerequisites ===
+    # Step 1 - Initialize the training configuration        
+        t_config = TrainingConfig
+        t_config.exp_name = 'exps/nowbar_hybrid'        
+    
+    # Step 2 - Select the desired model
+        model = NowbarHybrid(NowBarHybridConfig)
+        
+    # Step 3 - Initialize the input data object
+        input_data = InputDataNowBarHybrid(model)
+        
+    # Step 4 - Load training data
         path_x_train_bar = 'tra_X_bars'
         path_y_train_bar = 'tra_y_bars'
-        
-        # Nowbar
-        t_config.exp_name = 'exps/nowbar_hybrid'
-        model = NowbarHybrid(NowBarHybridConfig)
-        input_data = InputDataNowBarHybrid(model)
         input_data.add_data_sa(path_x_train_bar, path_y_train_bar, 'train') # x: input, y: conditional feature
-        # Temporal
-            # hybrid
-        ##t_config.exp_name = 'exps/temporal_hybrid'
-        ##model = TemporalHybrid(TemporalHybridConfig)
-        ##input_data = InputDataTemporalHybrid(model)
-        ##input_data.add_data_sa(path_x_train_bar, 'train')
         
-        # RNN
-            # composer
-        ##t_config.exp_name = 'exps/RNN_hybrid'
-        ##t_config.exp_name = 'exps/NowBarRNN_hybrid'
-        ##model = RNNHybrid(RNNHybridConfig)
-        ##model = RNNHybrid(NowBarRNNHybridConfig)
-        ##input_data = InputDataRNNHybrid(model)
-        ##input_data.add_data_sa(path_x_train_phr, 'train')
-        
+    # Step 5 - Initialize a museGAN object
         musegan = MuseGAN(sess, t_config, model)
-        ##musegan.load(musegan.dir_ckpt)
-        ##musegan.train(input_data)
-
-        ### load and generate samples ###
-        # load pretrained model
+        
+    # === Training ===
+        musegan.train(input_data)
+    
+    # === Load a Pretrained Model ===
         musegan.load(musegan.dir_ckpt)
     
-        # add testing data
-        ##path_x_test_phr = 'val_X_phrs'
+    # === Generate Samples ===
         path_x_test_bar = 'val_X_bars'
         path_y_test_bar = 'val_y_bars'
         input_data.add_data_sa(path_x_test_bar, path_y_test_bar, key='test')
-        ##input_data.add_data_sa(path_x_train_phr, key='test')
-    
-        # generate samples
         musegan.gen_test(input_data, is_eval=True)
 
 
