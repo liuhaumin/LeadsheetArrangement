ca3afd5c6b35bc7a490b2abfe67910a2348394f6
Hao-Min Liu (Paul) <liuhaumin@gmail.com>
2019-01-19 15:15:36 +0800

    Update store_sa.py

--- store_sa.py
+++ store_sa.py
@@ -18,16 +18,6 @@ def save_on_sa(data_dir, use_only_84_keys = True, rescale = True, postfix=''):
 
     for sd in subdirs:
         data = []
-        # lead sheet setting
-        ##for dp in range (2):
-        ##    x_name = data_prefix[dp]
-        ##    print (os.path.join(data_dir, sd , x_name+'.npy'))
-        ##    tmp_data =  np.reshape(np.load(os.path.join(data_dir, sd , x_name+'.npy')),(-1,384,128, 1))
-        ##    if(use_only_84_keys):
-        ##        tmp_data = tmp_data[:, :, 24:108, :]
-        ##    data.append(tmp_data)
-        ##
-        ##data_X = np.concatenate(data,axis = 3)
         
         # midi setting for training
 #         data_X = np.load(os.path.join(data_dir, sd, 'x_bar_chroma.npy'))
@@ -43,7 +33,7 @@ def save_on_sa(data_dir, use_only_84_keys = True, rescale = True, postfix=''):
         if sd is 'tra':
             data_y = np.load(os.path.join(data_dir, 'val' ,'y_bar_chroma.npy'))
         else:
-            data_y = np.load(os.path.join(data_dir, 'val' ,'y_bar_chroma_humanlifeleadsheet.npy'))
+            data_y = np.load(os.path.join(data_dir, 'val' ,'y_bar_chroma_test.npy'))
             #data_y = np.load(os.path.join(data_dir, sd ,'y_bar_chroma.npy'))
 
         print(data_X.dtype)
@@ -68,28 +58,34 @@ def save_on_sa(data_dir, use_only_84_keys = True, rescale = True, postfix=''):
         tmp_arr_y = sa.create(name, data_y.shape, dtype=float)
         np.copyto(tmp_arr_y, data_y)
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    ##save_on_sa('../../wayne/v3.0/dataset/data_bar', postfix='bars')
-    ##save_on_sa('./data_tab_4dbar_12', postfix='phrs')
-    ##save_on_sa('./data/chroma_roll', postfix='bars')
-    ##save_on_sa('./data/chroma_beat', postfix='bars')
-    print('try')
-    try:
-        sa.delete('tra_X_bars')
-    except:
-        print('error tra_X_bars')
-    try:
-        sa.delete('tra_y_bars')
-    except:
-        print('error tra_y_bars')
-    try:
-        sa.delete('val_X_bars')
-    except:
-        print('error val_X_bars')
-    try:
-        sa.delete('val_y_bars')
-    except:
-        print('error val_y_bars')
-    
-    save_on_sa('./data/chord_roll', postfix='bars')
+print(' loading data !!')
+
+try:
+    sa.delete('tra_X_bars')
+except:
+    # print('error tra_X_bars')
+    pass
+try:
+    sa.delete('tra_y_bars')
+except:
+    # print('error tra_y_bars')
+    pass
+try:
+    sa.delete('val_X_bars')
+except:
+    # print('error val_X_bars')
+    pass
+try:
+    sa.delete('val_y_bars')
+except:
+    # print('error val_y_bars')
+    pass
+
+save_on_sa('./data/chord_roll', postfix='bars')
+print('----------loading data completed !!--------------')
+##save_on_sa('../../wayne/v3.0/dataset/data_bar', postfix='bars')
+##save_on_sa('./data_tab_4dbar_12', postfix='phrs')
+##save_on_sa('./data/chroma_roll', postfix='bars')
+##save_on_sa('./data/chroma_beat', postfix='bars')
